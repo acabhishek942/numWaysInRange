100e8a5c6ef3adceee7005f11738891de9dd60c6
Abhishek Choudhary <acabhishek942@gmail.com>
2018-06-04 22:23:02 +0530

    now solving for less than 3 values of x

--- numWaysInRange.py
+++ numWaysInRange.py
@@ -6,15 +6,15 @@ def ncr(n, r):
     denom = reduce(op.mul, range(1, r+1), 1)
     return numer//denom
 
-    
+
 numTestCases = int(input())
 LRlist = []
 for _ in range(numTestCases):
     LRlist.append(input().split())
 
-# print (LRlist
 for i in LRlist:
+    numWays = 0
     for x in range(int(i[0]), int(i[1])+1):
-        print ("x is ", x)
         if x < 3:
-            print (2 * ncr(int(i[1], x)))
+            numWays += 2 * ncr(int(i[1]), x)
+    print(numWays)
