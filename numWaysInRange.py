4cc2bbe9673452cc2607900f917e4c384bf36a35
Abhishek Choudhary <acabhishek942@gmail.com>
2018-06-04 21:54:22 +0530

    correctly call the ncr function

--- numWaysInRange.py
+++ numWaysInRange.py
@@ -6,14 +6,15 @@ def ncr(n, r):
     denom = reduce(op.mul, range(1, r+1), 1)
     return numer//denom
 
+    
 numTestCases = int(input())
 LRlist = []
 for _ in range(numTestCases):
     LRlist.append(input().split())
 
-# print (LRlist)
-
+# print (LRlist
 for i in LRlist:
     for x in range(int(i[0]), int(i[1])+1):
+        print ("x is ", x)
         if x < 3:
-            print (2 * ncr(x, int(i[1])))
+            print (2 * ncr(int(i[1], x)))
