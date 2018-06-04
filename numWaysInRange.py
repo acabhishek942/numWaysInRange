4411954746e94d954f50122627b1daffee44e53a
Abhishek Choudhary <acabhishek942@gmail.com>
2018-06-04 21:47:37 +0530

    wrong code for sharing

--- numWaysInRange.py
+++ numWaysInRange.py
@@ -1,6 +1,19 @@
+import operator as op
+from functools import reduce
+def ncr(n, r):
+    r = min(r, n-r)
+    numer = reduce(op.mul, range(n, n-r, -1), 1)
+    denom = reduce(op.mul, range(1, r+1), 1)
+    return numer//denom
+
 numTestCases = int(input())
 LRlist = []
 for _ in range(numTestCases):
     LRlist.append(input().split())
 
-print (LRlist)
+# print (LRlist)
+
+for i in LRlist:
+    for x in range(int(i[0]), int(i[1])+1):
+        if x < 3:
+            print (2 * ncr(x, int(i[1])))
